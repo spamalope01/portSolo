9de33e1fd36969d9ffa9b19d2e2dedf4c9e1ae8e
spamalope01 <spamalope01@yahoo.com>
2016-06-01 21:23:01 -0700

    initial creation of files and basic pseudo code to brainstorm what to do with this project.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Initial creation of READMDE file.  
+
+#Updated app.js and index.html with pseudo code brainstorming things i would need for the portfolio site.  
---
+++ app.js
@@ -0,0 +1,5 @@
+// i'm going to need a constructor.  I want to create a slideshow of projects that i've done.  each slide in the slideshow will represent a single project.  so the constructor is going to need things like: title, author, date published to the web, client name, description, languages/technologies used.  i can add to that later if i think of other qualities.
+
+// next i'll need...maybe not a prototype, but some function that pushes items through the constructor so that my slideshow can populate.
+
+// finally, naturally, i'll have to call these functions at the bottom like we did with the blog project.  
